eec0f7ec31cb1f48cccc9a6d24e13925ae0ac85a
Jean Caille <jcaille@gmail.com>
2020-01-01 18:28:19 +0100

    Returning a better version of the serializer

--- gyft_storage/gyft/serializers.py
+++ gyft_storage/gyft/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import GiftList
 
+class UserSerialiser(serializers.ModelSerializer):
+	
+	class Meta:
+		model = User
+		fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
+
 class GiftListSerialiser(serializers.ModelSerializer):
+
+	recipient = UserSerialiser(read_only = True)
+
 	class Meta:
 		model = GiftList
 		fields = ('id', 'title', 'description', 'recipient')
