e2cf8da69f2013c88f017d7c1cefd27b3d490e83
Jean Caille <jcaille@gmail.com>
2020-01-01 17:38:25 +0100

    Finishing tutorial

---
+++ gyft_storage/gyft/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import GiftList
+
+class GiftListSerialiser(serializers.ModelSerializer):
+	class Meta:
+		model = GiftList
+		fields = ('id', 'title', 'description')
--- gyft_storage/gyft/views.py
+++ gyft_storage/gyft/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import GiftListSerialiser
+from .models import GiftList
 
 # Create your views here.
+
+class GiftListView(viewsets.ModelViewSet):
+	serializer_class = GiftListSerialiser
+	queryset = GiftList.objects.all()
